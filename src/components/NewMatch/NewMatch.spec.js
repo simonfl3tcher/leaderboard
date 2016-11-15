772e4cedd83228d662994ab0f24b99acd710b797
Simon Fletcher <simonfletcher0@gmail.com>
2016-11-15 10:22:20 +0000

    additional match specs

--- src/components/NewMatch/NewMatch.spec.js
+++ src/components/NewMatch/NewMatch.spec.js
@@ -1,10 +1,17 @@
 // JS
+import 'jsdom-global/register'
 import React              from 'react';
+import { Provider }       from 'react-redux';
+import * as router        from 'react-router';
 import { mount, shallow } from 'enzyme';
 import { expect }         from 'chai';
+import configureStore     from 'redux-mock-store'
+import sinon              from 'sinon';
+
+const mockStore = configureStore([])
 
 // Components
-import { NewMatch }       from './NewMatch';
+import ConnectedApp, { NewMatch }       from './NewMatch';
 
 const match = {
   title: "BSE vs Stowmarket",
@@ -13,10 +20,41 @@ const match = {
 }
 
 describe("<NewMatch />", () => {
+  let Component;
+
+  beforeEach(() => {
+    const initialState = {}
+    const store = mockStore(initialState)
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <ConnectedApp />
+      </Provider>
+    );
+
+    Component = wrapper.find(NewMatch);
+  });
+
+  it("should render", () => {
+    expect(
+      Component.length
+    ).to.be.truthy;
+  });
+
   it("should have the string 'Create Match here!' ", () => {
     const wrapper = shallow(<NewMatch match={match}/>);
     expect(
       wrapper.contains("Create Match!")
     ).to.equal(true);
+  });
+
+  it("should change the route on onSave", () => {
+    router.browserHistory = { push: ()=>{} };
+    let browserHistoryPushStub = sinon.stub(router.browserHistory, 'push', () => { });
+
+    Component.find('button').simulate('click');
+
+    expect(browserHistoryPushStub).to.have.been.calledOnce;
+    browserHistoryPushStub.restore();
   });
 });
